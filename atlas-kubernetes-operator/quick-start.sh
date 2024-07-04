f7aabd12ac9dae65a27e4505a075f033be4fb96b
karl-denby <karl.denby@gmail.com>
2024-07-04 11:58:45 +0100

    works on linux

--- atlas-kubernetes-operator/quick-start.sh
+++ atlas-kubernetes-operator/quick-start.sh
@@ -1,10 +1,66 @@
 #!/bin/bash
+export AKO_namespace=mongodb-atlas-system
+
+if [[ ! -v $AKO_ORGID ]]; then
+  read -r -p "Please provide your Atlas Organization ID: " AKO_ORGID
+fi
+
+if [[ ! -v $AKO_PROJID ]]; then
+  read -r -p "Please provide your Atlas Project ID: " AKO_PROJID
+fi
+
+if [[ ! -v $AKO_PUBKEY ]]; then
+  read -r -p "Please provide your API Public Key: " AKO_PUBKEY
+fi
+
+if [[ ! -v $AKO_PRIKEY ]]; then
+  read -r -p "Please provide your API Private Key: " AKO_PRIKEY
+fi
+
+if [[ ! -v $AKO_PROJNAME ]]; then
+  read -r -p "Please provide your existing Project Name: " AKO_PROJNAME
+fi
+
+### Prompt for version
+
+version_options=("v2.3.1" "v2.2.2" "v2.0.1" "Quit")
+select opt in "${version_options[@]}"
+do
+  case $opt in
+      v2.3.1)
+      export AKO_version=v2.3.1
+      break
+      ;;
+      v2.2.2)
+      export AKO_version=v2.2.2
+      break
+      ;;
+      v2.0.1)
+      export AKO_version=v2.0.1
+      break
+      ;;
+      # v1.9.3)
+      # export AKO_version=v1.9.3
+      # break
+      # ;;
+      Quit)
+      echo "Bye."
+      break
+      ;;
+      *)
+      echo "Invalid option"
+      ;;
+  esac
+done
+
+### Setup Kubernetes Cluster
+
 if ! minikube &> /dev/null
 then 
   echo "Minikube could not be found, please install."
   exit 1
 else
-  minikube start --cpus=2 --memory=2G --disk-size=5000mb -p atlas
+  minikube start --cpus=2 --memory=2G --disk-size=5000mb -p atlas --namespace $AKO_namespace
   echo "=== Kubernetes Setup ==="
   echo "- profile = atlas"
   echo "- CPUs = 2"
@@ -63,10 +119,23 @@ then
 fi
 
 echo "=== Using Atlas CLI to setup kubernetes operator"
-kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-atlas-kubernetes/v2.2.2/deploy/all-in-one.yaml
+kubectl apply -f https://raw.githubusercontent.com/mongodb/mongodb-atlas-kubernetes/$AKO_version/deploy/all-in-one.yaml
 echo "Operator is installed"
 echo ""
-kubectl get pods -n mongodb-atlas-system
-echo ""
-echo "Please visit https://www.mongodb.com/docs/atlas/operator/stable/ak8so-quick-start/ to continue"
+kubectl get pods -n $AKO_namespace
+awk -v AKO_ORGID="$AKO_ORGID" \
+    -v AKO_PROJID="$AKO_PROJID" \
+    -v AKO_PUBKEY="$AKO_PUBKEY" \
+    -v AKO_PRIKEY="$AKO_PRIKEY" \
+    -v AKO_PROJNAME="$AKO_PROJNAME" \
+    -v AKO_IPACCESS=$(curl icanhazip.com) \
+    -v AKO_KEEPUNTIL=$(date  +%F -d "+1 days") \
+    -f setup.awk deploy-a-cluster-template.yaml > deploy-a-cluster.yaml
+#kubectl apply -f deploy-a-cluster.yaml
+echo
+echo "Cluster can been deployed using the command:"
+echo "$ kubectl apply -f deploy-a-cluster.yaml"
+echo
+echo "Please modify the above file to your requirments before running"
+
 exit 0
